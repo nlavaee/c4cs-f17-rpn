fa115ff8970c9af34160ea23b7faa77d79cd6d34
nlavaee <nlavaee@umich.edu>
2017-11-08 12:37:45 -0500

    Update test_rpn.py

--- test_rpn.py
+++ test_rpn.py
@@ -10,3 +10,12 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+		
+	def test_multiply(self):
+        	result = rpn.calculate("-5 3 *")
+        	self.assertEqual(-15, result)
+		
+    	def test_divide(self):
+        	result = rpn.calculate("8 4 /")
+        	self.assertEqual(2, result)
+
