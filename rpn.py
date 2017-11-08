63c40c2e4acb83f469434b22224fa9671d32924f
nlavaee <nlavaee@umich.edu>
2017-11-08 15:27:40 -0500

    Update rpn.py

--- rpn.py
+++ rpn.py
@@ -32,8 +32,7 @@ def calculate(myarg):
 def main():
     while True:
         result = calculate(input("rpn calc> "))
-        print('\033[31m' + 'some red text')
-        print("Result: ", result)
+        print('\033[31m' + "Result: ", result)
 
 if __name__ == '__main__':
     main()
